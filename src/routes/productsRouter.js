40d45e30b34fcc414797cf56e4476558bd08b758
Rayanderson Sousa <rayandersonsousa@pop-os.localdomain>
2023-01-18 15:39:58 -0300

    Requisito 01 fix

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -6,6 +6,4 @@ const router = express.Router();
 router.get('/', productsController.getAll);
 router.get('/:id', productsController.getById);
 
-module.exports = {
-  router,
-};
+module.exports = router;
